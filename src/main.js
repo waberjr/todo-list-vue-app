c38127758a3c09cf527a27917ade030cd7dcecf0
Waber Júnior <waber.junior@madefy.com.br>
2023-04-30 20:35:46 -0300

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
+import store from './store'
+import App from './App.vue'
 
 import './assets/main.css'
 import './assets/css/tailwind.css'
 
-const app = createApp(App)
+const app = createApp(App).use(store)
 
 app.use(router)
 
