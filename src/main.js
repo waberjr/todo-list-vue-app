299b31956692a1fa363dd57b0f87100ceb0fbef5
Waber Júnior <waber.junior@madefy.com.br>
2023-04-30 21:04:02 -0300

    add package axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import router from './router'
-import store from './store'
-import App from './App.vue'
+import router from '@/router'
+import store from '@/store'
+import App from '@/App.vue'
 import '@/plugins/vee-validate.js';
+import '@/plugins/axios.js';
 
 import '@/assets/main.css'
 import '@/assets/css/tailwind.css'
